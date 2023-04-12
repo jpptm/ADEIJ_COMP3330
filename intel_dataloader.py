4b63e177351f5aa8f0a32a765898e07d2a44dede
jpptm <prncmontano@gmail.com>
2023-04-12 22:54:43 +1000

    Working dataset parser

--- intel_dataloader.py
+++ intel_dataloader.py
@@ -5,13 +5,18 @@
 
 import os
 
+import cv2
 import numpy as np
 import torch
-import cv2
+import torchvision as tv
+
 
 # Assumes that the given path contains all the folders with all the data inside it
+# NOTE: We take the image names and process this instead of loading everything at once into memory. This way, we make sure we don't exhaust our memory and crash the program
+# NOTE: This class should work from both training and validation sets, as long as the right path is given
 class IntelDataLoader(torch.utils.data.Dataset):
     def __init__(self, data_path):
+        # Put all the classes that we know into a tuple
         self.known_classes = (
             "buildings",
             "forest",
@@ -21,37 +26,61 @@ class IntelDataLoader(torch.utils.data.Dataset):
             "street",
         )
 
+        # Gaussian Blur
+        # Random Invert?
+        # Random Posterize
+        # Random Adjust Sharpness
+        # Random Autocontrast
+        # Random Equalize
+        # Random Solarize?
+
+        # Random crop (Not quite sure about this one)
+        # Vertical flip (Not quite sure about this one, it could be detrimental to the network's performance)
+        # Horizontal flip
+        # Random Perspective
+        # Random affine
+
+        # Normalise
+
+        # Data augmentations to be applied
+        transforms = []
+
         self.master_data = []
+
+        # Go through each subdirectory, generate a label for each entry and add it to the master data
         for idx, member in enumerate(self.known_classes):
             current_path = os.path.join(data_path, member)
-            no_imgs = len(os.listdir(current_path))
-
             full_names = [
                 os.path.join(current_path, img) for img in os.listdir(current_path)
             ]
 
-            # cv2.imshow("image", cv2.resize(cv2.imread(full_names[100]), (450, 450)))
-            # cv2.waitKey(0)
-            # print(full_names)
-            # exit(0)
-
+            no_imgs = len(os.listdir(current_path))
             labels = np.ones(no_imgs) * idx
 
+            # Bind labels and image names together then add each to the master data list
             for item in zip(full_names, labels):
                 self.master_data.append(item)
-
-        print(len(self.master_data))
-
-        for data in self.master_data:
-            print(data)
-            input("...")
 
     def __len__(self):
         return len(self.master_data)
 
-    def __get_item__(self):
-        pass
+    def __get_item__(self, idx):
+        # Get the image name and label
+        img_name, label = self.master_data[idx]
+
+        # Read the image and convert it to a tensor
+        img = cv2.imread(img_name)
+        img = torch.from_numpy(img)
+        print(img)
+        # Convert the label to a tensor
+        label = torch.tensor(label)
+
+        return img, label
 
 
-path = os.path.join(os.getcwd(), "..", "ADEIJ_datasets", "seg_test", "seg_test")
-IntelDataLoader(path)
+# Ideas on how to use
+# path = "C:/Microsoft VS Code/ADEIJ_datasets/seg_train/seg_train"
+# s = IntelDataLoader(path)
+# print(torch.Tensor.size(s.__get_item__(0)[0].permute(2, 0, 1)))
+# dataset = IntelDataLoader(seg_train_path)
+# DataLoader(dataset, batch_size=32, shuffle=True)
