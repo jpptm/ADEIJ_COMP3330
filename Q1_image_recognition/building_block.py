cb67820eb0f405fad94de168c83fdd10833dcade
A Herfel <angelherf@gmail.com>
2023-04-30 02:16:45 +1000

    just get this stuff off my pc

---
+++ Q1_image_recognition/building_block.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch
+from torch import Tensor
+from typing import Type
+
+class BasicBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        stride: int = 1,
+        expansion: int = 1,
+        downsample: nn.Module = None
+    ):
+        super(BasicBlock, self).__init__()
+        # Multiplicative factor for the subsequent conv2d layer's output channels.
+        # It is 1 for ResNet18 and ResNet34.
+        self.expansion = expansion
+		# Optionally reduce the spatial dimensions of the input tensor to match output
+		# Helps reduce computational complexity and capture more global features
+        self.downsample = downsample
+
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(
+            out_channels,
+            out_channels*self.expansion,
+            kernel_size=3,
+            padding=1,
+            bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels*self.expansion)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+		# shortcut connection
+		# helps the network decide how much or little information should
+		# be passed to the next layer
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out += identity
+        out = self.relu(out)
+        return  out
